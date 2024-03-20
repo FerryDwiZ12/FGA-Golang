9ca063163cc4f8dc053319d775aaad613fa76e43
Ferry Dwi Zulkhifli <ferrydwizulkhifli@gmail.com>
2024-03-20 20:48:44 +0700

    assignment 3

---
+++ assigments/assignment3/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/FerryDwiZ12/FGA-Golang/tree/master/assigments/helpers"
+)
+
+func main() {
+	fmt.Println("Hello world")
+
+	helpers.RunCronJob()
+
+	// fmt.Println(helpers.RandomNum(1, 100))
+
+}
